51fd7c24c5b7f7c32c451004851fa54336b80380
Natnael Eyuel <natnael.eyuel.dev@gmail.com>
2025-07-16 15:54:08 +0300

    I have completed Building a Task Manager RESTful API with Go and  Gin following all the requirements.

--- models/task.go
+++ models/task.go
@@ -7,9 +7,9 @@ import (
 
 // task item
 type Task struct {
-	ID            int         `json:"id"`                     								   // unique identifier of task
-	Title         string      `json:"title"`                  								   // title of task
-	Description   string      `json:"description"`            								   // description of task
-	DueDate       time.Time   `json:"due_date" time_format:"2006-01-02T15:04:05Z"`  		   // due date of task (ISO 8601 format)
-	Status        string      `json:"status" binding:"oneof=pending in_progress completed"`    // status of task
+	ID            int         `json:"id"`                     				    // unique identifier of task
+	Title         string      `json:"title"`                  				    // title of task
+	Description   string      `json:"description"`            				    // description of task
+	DueDate       time.Time   `json:"due_date" time_format:"2006-01-02T15:04:05Z"`  	    // due date of task (ISO 8601 format)
+	Status        string      `json:"status" binding:"oneof=pending in_progress completed"`     // status of task
 }
